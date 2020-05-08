a388d4558dd6ce0fa8dd88b5426c3d9c193b9b58
TanimaRanjan <tanimaranjan@gmail.com>
2020-05-08 09:17:34 -0400

    Combining useStates in to useReducer for comics

--- src/reducers/comicsReducer.js
+++ src/reducers/comicsReducer.js
@@ -1,9 +1,30 @@
 const comicsReducer = (state, action) => {
     switch(action.type) {
-        case 'SET_COMICS':
-            return action.payload;
+        case 'COMIC_FETCH_INIT':
+            return {
+                ...state, 
+                isLoading:true,
+                isError:false
+            };
+        case 'COMIC_FETCH_SUCCESS':
+            return {
+                ...state,
+                isLoading:false,
+                isError:false,
+                data:action.payload
+            };
+        case 'COMIC_FETCH_ERROR':
+            return {
+                ...state,
+                isLoading:false,
+                isError:true
+            }
         case 'REMOVE_COMIC':
-            return state.filter(comic => action.payload.id !== comic.id);
+            return {
+                ...state,
+                data:state.filter(comic => action.payload.id !== comic.id)
+            }
+            
         default:
             throw new Error()
         
